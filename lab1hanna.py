16e35df8fe4a26933ff57f29674726fd84761abe
6305190 <6305190@edu.vaniercollege.qc.ca>
2025-09-02 14:08:00 -0400

    Add files via upload

---
+++ lab1hanna.py
@@ -0,0 +1,18 @@
+print ("Hello,world!")
+def input_output():
+
+#getting input from the user
+    name = input("Enter your name: ")
+
+    age = int(input("Enter your ageH: "))
+
+    height = float(input("Enter your height: "))
+
+ #outputting the collected data
+    print(f"Hello, {name}!")
+
+    print(f"You are {age} years old.")
+
+    print(f"Your height is {height} meters.")
+    
+input_output()
