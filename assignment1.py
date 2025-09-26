a463a3fdc1bcb0a79cacd9a36ad942abdc80843b
6305190 <6305190@edu.vaniercollege.qc.ca>
2025-09-26 14:17:16 -0400

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,24 @@
+#Question 1 using pythons built in function
+a = input ("Enter a")
+b = input ("Enter b")
+c = input ("Enter c")
+numbers = [a,b,c]
+max_value = max(numbers)
+
+print(max_value)
+
+#Question 2 using pythons built in function
+a = input ("Enter a")
+b = input ("Enter b")
+c = input ("Enter c")
+numbers = [a,b,c]
+min_value = min(numbers)
+
+print(min_value)
+
+
+
+
+
+
+              
